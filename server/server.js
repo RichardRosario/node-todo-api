5e2b4f54f74fdb8c83b02eef024f7e4b16a1e60e
Richard <catrosits@gmail.com>
2018-06-26 20:39:18 +0800

    add user route and token method

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.post('/todos', (req, res) => {
    })
 });
 
-app.get('/todos', (req, res) => {
+ app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos})
     }, (err) => {
@@ -97,10 +97,12 @@ body.completedAt = new Date().getTime();
 
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
-
     var user = new User(body);
-    user.save().then((user) => {
-        res.send(user);
+    
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header(x-auth, token).send(user);
     }).catch((err) => {
         res.status(400).send(err);
     });
