b4bf3381707bca56f105970fe82a2361186ecef5
Richard <catrosits@gmail.com>
2018-06-26 14:18:37 +0800

    add delete post

--- server/server.js
+++ server/server.js
@@ -51,6 +51,27 @@ app.get('/todos/:id', (req, res) => {
     })
 });
 
+//delete route
+app.delete('/todos/:id', (req, res) => {
+    //get id
+    var id = req.params.id;
+
+    //validate id return 404 if not valid
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    //remove by id
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(400).then();
+        }
+        res.send(todo);
+    }).catch((err) => {
+        res.status(404).send();
+    })
+});
+
 app.listen(port, () => {
     console.log(`Started at port ${port}`);
 });
